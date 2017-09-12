982fc25e0a698a87e2a2a9c1a553acb3432d5d98
Nero <louis.vanbiervliet@gmail.com>
2017-09-12 09:32:49 +0200

    distribution generation is now deterministic

--- columbus/datatypes.py
+++ columbus/datatypes.py
@@ -7,6 +7,7 @@ class Normal(DataItem):
 	
 	# constructor
 	def __init__(self, simulator, mu, sigma):
+		np.random.seed(0)
 		super(Normal, self).__init__(simulator, np.random.normal(mu, sigma, simulator.finesse))
 
 # scalar number
@@ -26,6 +27,7 @@ class Skewed(DataItem):
 
 		mu = math.log(max(abs(median),0.001)) # NEED max is nu omdat anders faalt op median = 0
 		sigma = skewness
+		np.random.seed(0)
 		samples = np.random.lognormal(mu, sigma, simulator.finesse)
 		if tailDirection == 'left': # mirror samples around median
 			samples = np.subtract(2*abs(median),samples)		
@@ -39,4 +41,5 @@ class Uniform(DataItem):
 	
 	# constructor
 	def __init__(self, simulator, lo, hi):
+		np.random.seed(0)
 		super(Uniform, self).__init__(simulator, np.random.uniform(lo, hi, simulator.finesse))
